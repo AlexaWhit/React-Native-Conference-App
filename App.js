31fa0bec6a426b63efcd22d30d85c36129ab7ba1
Alexa Whitney <alexa.whitney@shopify.com>
2022-12-12 12:10:34 -0700

    completed DetailsScreen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <DetailsScreen />
     </View>
   );
 }
