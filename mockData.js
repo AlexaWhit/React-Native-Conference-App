14f6e85a5a9ad7b4347a072c4f661cf255d15626
Alexa Whitney <alexa.whitney@shopify.com>
2022-12-13 14:07:45 -0700

    completed conference app workshop

--- mockData.js
+++ mockData.js
@@ -23,9 +23,9 @@ export const mockData = [
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
         time: "9:30 AM",
         speaker: {
-          name: "Haris Mahmood",
+          name: "Harriet Mahmood",
           role: "Chief Millenial Officer",
-          avatar: "https://api.adorable.io/avatars/285/harismahmood.png"
+          avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQPYhh8sXW9McB1yVdGS48IQNwo0E4Go4WzQ&usqp=CAU"
         }
       }
     ]
@@ -43,7 +43,7 @@ export const mockData = [
         speaker: {
           name: "Ryan Baldwin",
           role: "Baker of Bytes",
-          avatar: "https://api.adorable.io/avatars/285/ryan.png"
+          avatar: "https://freesvg.org/img/1547510251.png"
         }
       },
       {
@@ -55,7 +55,7 @@ export const mockData = [
         speaker: {
           name: "Sergey Gavrilyuk",
           role: "Hardcore Canadian",
-          avatar: "https://api.adorable.io/avatars/285/sergey.png"
+          avatar: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
         }
       },
       {
@@ -65,9 +65,9 @@ export const mockData = [
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
         time: "10:15 AM",
         speaker: {
-          name: "Corey Pollock",
+          name: "Cassidy Pollock",
           role: "Poor man's Steve Jobs",
-          avatar: "https://api.adorable.io/avatars/285/corey.png"
+          avatar: "https://cdn-icons-png.flaticon.com/512/194/194938.png"
         }
       }
     ]
@@ -85,7 +85,7 @@ export const mockData = [
         speaker: {
           name: "Jacob Abraham",
           role: "Founder & CEO of Jacob By Jacob",
-          avatar: "https://api.adorable.io/avatars/285/jacob.png"
+          avatar: "https://cdn-icons-png.flaticon.com/512/147/147142.png"
         }
       },
       {
@@ -95,9 +95,9 @@ export const mockData = [
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
         time: "11:00 AM",
         speaker: {
-          name: "Matt Legaspi",
+          name: "Sarah Legaspi",
           role: "Figma World Champion",
-          avatar: "https://api.adorable.io/avatars/285/matt.png"
+          avatar: "https://t4.ftcdn.net/jpg/02/45/28/17/360_F_245281721_2uYVgLSFnaH9AlZ1WWpkZavIcEEGBU84.jpg"
         }
       },
       {
@@ -109,7 +109,7 @@ export const mockData = [
         speaker: {
           name: "Mo Hashi",
           role: "Director of Business",
-          avatar: "https://api.adorable.io/avatars/285/mo.png"
+          avatar: "https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small_2x/man-avatar-icon-free-vector.jpg"
         }
       }
     ]
@@ -141,7 +141,7 @@ export const mockData = [
         speaker: {
           name: "Lloyd Wiredu",
           role: "React Native Instagram Influencer",
-          avatar: "https://api.adorable.io/avatars/285/lloyd.png"
+          avatar: "https://cdn-icons-png.flaticon.com/512/147/147144.png?w=360"
         }
       }
     ]
